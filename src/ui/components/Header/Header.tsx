5a15a682044c9c9ea3667bcc2c86571a4f9b2add
Rexhin Hoxha <rexhin@icloud.com>
2026-03-08 00:18:49 +0100

    feat(header): Display environment badge in header

--- src/ui/components/Header/Header.tsx
+++ src/ui/components/Header/Header.tsx
@@ -6,11 +6,13 @@ import { FaCartShopping } from "react-icons/fa6";
 
 export default function Header() {
   const toggleOverlay = useAppStore((state) => state.toggleOverlay);
+  const env = import.meta.env.VITE_ENV;
 
   return (
     <header className="p-4">
-      <Link to="/">
+      <Link className="relative" to="/">
         <h1 className="text-2xl font-bold">Fruits App</h1>
+        <span className="badge">{env}</span>
       </Link>
 
       <div className="flex items-center gap-4">
